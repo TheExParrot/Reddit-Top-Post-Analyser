6390a27ca9b44cda83d6d64ee50de67f6d302397
Jack Perry <125957065+TheExParrot@users.noreply.github.com>
2023-02-24 13:20:19 +1100

    Added comment content extractor

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Magic Numbers
 NUMBER_OF_POSTS = 10
-NUMBER_OF_COMMENTS = 10
+NUMBER_OF_COMMENTS = 20
+
+
+# Function that returns the top x comments of the post
+def get_comments_text(post_url, num_comments):
+    comment_response = requests.get(post_url, headers={'User-agent': 'Mozilla/5.0'})
+    comment_soup = BeautifulSoup(comment_response.content, 'html.parser')
+    comments = comment_soup.find_all('div', {'class': 'md'})
+    comments_list = []
+    for i in range(1, num_comments + 1):
+        comments_list.append(comments[i].text.strip())
+    return comments_list
+
 
 # Get the website using the requests library
 url = "https://old.reddit.com/r/worldnews/top/?sort=top&t=week"
@@ -16,7 +29,7 @@ posts = soup.find_all("div", class_="top-matter")
 non_promoted_posts = [post for post in posts if "promoted by"
                       not in post.find("p", class_="tagline").text.strip()]
 
-# Get a list of the top posts URLs
+# Iterate through the top x posts and return the content of the top y comments
 urls = []
 for i in range(NUMBER_OF_POSTS):
     urls.append(non_promoted_posts[i].find("a", class_="bylink comments may-blank")['href'])
